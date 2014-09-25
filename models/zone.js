360e07d5531aa06df55f88552b0809a132d3af1f
Nicolas Ronchi <narc88@gmail.com>
2014-09-25 12:39:33 -0300

    Sossosososoos

--- models/zone.js
+++ models/zone.js
@@ -1,15 +1,15 @@
 var validator = require('validator');
 var mongoose = require('mongoose');
 
-var GroupSchema = new mongoose.Schema({
+var ZoneSchema = new mongoose.Schema({
     matchday_number	: { 
 				type: Number,
 				required:true
             },
     dispute_day     : {type: Date, default: Date.now },
-    matches     : [{type: mongoose.Schema.ObjectId, ref: 'Matchday' }],
+    matchdays     : [{type: mongoose.Schema.ObjectId, ref: 'Matchday' }],
 	created    	: {type: Date, default: Date.now },
 	modified	: {type: Date, default: Date.now }
 });
 
-exports.GroupModel = mongoose.model('Group', GroupSchema);
+exports.ZoneModel = mongoose.model('Zone', ZoneSchema);
