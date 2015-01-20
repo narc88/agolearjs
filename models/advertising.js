0125a57ae7c95d27ef16960510d348380265fa86
Nicolas Ronchi <narc88@gmail.com>
2015-01-20 09:10:43 -0300

    sorete

--- models/advertising.js
+++ models/advertising.js
@@ -1,12 +1,14 @@
 // Creación de la Conexión
 var mongoose = require('mongoose');
-
+var ImageSchema = require('../models/image').ImageSchema;
+var validator = require('validator');
 
 var AdvertisingSchema = new mongoose.Schema({
 	title				: { type: String, required:true, trim:true},
 	description				: { type: String, trim:true},
 	link				: { type: String, required:true, trim:true},
 	expiration        		: { type: Date, default: Date.now },
+	images 				: [ImageSchema],
 	created        		: { type: Date, default: Date.now },
 	modified       		: { type: Date, default: Date.now }
 });
